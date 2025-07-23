0e89a811fe7c710f2496fe1db1b7385098d8b2bc
PAZZO123 <smbabazipatrick@gmail.com>
2025-07-23 13:53:48 +0200

    'J22'

--- spread.js
+++ spread.js
@@ -2,5 +2,6 @@ var arr=[1,2,3]
 var newArr=[...arr,3,4,5,5]//to make shallow copy
 var add=[4,5,6,6]
 var comb=[...arr , ...add]//combine two array
+var text=[...'Hello pazzo']
 console.log(Math.max(...newArr))//to iterate over array 
-console.log(comb)
+console.log(text)
